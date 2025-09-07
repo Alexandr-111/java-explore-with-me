7e1879f3879773ba451098bacb16dff0ade2f8d0
Aleksandr Sovetnikov <ALEX-6-3-7@yandex.ru>
2025-09-07 07:00:53 +0300

    Финальный проект. Вторая часть_7

--- statistics-service/client-stat-service/src/main/java/ru/practicum/statsclient/StatsClientController.java
+++ statistics-service/client-stat-service/src/main/java/ru/practicum/statsclient/StatsClientController.java
@@ -1,9 +1,14 @@
 package ru.practicum.statsclient;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -52,6 +57,21 @@ public class StatsClientController {
         log.debug("StatsController. Запрос статистики. Получены объекты start {}, end {}, uris {} и unique {}",
                 start, end, uris, unique);
 
-        return statsClient.fetchStats(start, end, uris, unique);
+        List<ViewStatsDto> result = statsClient.fetchStats(start, end, uris, unique);
+
+        try {
+            byte[] bodyBytes = new ObjectMapper()
+                    .registerModule(new JavaTimeModule())
+                    .writeValueAsBytes(result);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.setContentLength(bodyBytes.length);
+
+            return new ResponseEntity<>(result, headers, HttpStatus.OK);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка при сериализации ответа", e);
+        }
     }
 }
